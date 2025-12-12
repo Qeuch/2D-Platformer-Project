f2820e8f7fa5d11a72d04798d1a9add9a54e9c81
Chad Watson <chadwatson2005@gmail.com>
2025-12-12 15:49:17 -0500

    Added Level Save Feature

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -4,8 +4,10 @@ public class NextLevel : MonoBehaviour
 {
 
     public string nextLevelName;
+    public int nextLevelValue;
     public void LoadNextLevel()
     {
+        PlayerPrefs.SetInt("LevelReached", nextLevelValue);
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevelName);
         Time.timeScale = 1;
     }
